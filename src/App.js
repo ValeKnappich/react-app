60ebf2fc20f8b806cd66e9ee04db8767920dc099
Valentin Knappich <valentin.knappich@web.de>
2018-10-12 18:02:27 +0200

    Save and reload scoreboard entries

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import '../node_modules/grommet-css';
 import {Tabs, Tab} from 'grommet';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.store = {vale: {scoreboard_entries: null}};
+    }
     render() {
         return (
             <Tabs>
                 <Tab title="Vale">
-                    <ValesMiniGame/>
+                    <ValesMiniGame parent={this}/>
                 </Tab>
                 <Tab title="Markus">
 
