b0a6a9446977bc9bfbcfb158a805e44e2e9e89a5
Valentin Knappich <valentin.knappich@web.de>
2018-10-19 09:47:11 +0200

    Updates

---
+++ precache-manifest.789c04ca764cc373d0128e342318393c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "49631b63d5fca3be563f0336dde7d20e",
+    "url": "/react-app/static/media/funny_bot.49631b63.gif"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "a507e47dfe73c3010ca0",
+    "url": "/react-app/static/js/main.a507e47d.chunk.js"
+  },
+  {
+    "revision": "dcf985aea1173c78a278",
+    "url": "/react-app/static/js/1.dcf985ae.chunk.js"
+  },
+  {
+    "revision": "a507e47dfe73c3010ca0",
+    "url": "/react-app/static/css/main.04a611ad.chunk.css"
+  },
+  {
+    "revision": "dcf985aea1173c78a278",
+    "url": "/react-app/static/css/1.ebddad0e.chunk.css"
+  },
+  {
+    "revision": "311e37db1fbe10d71927ad7fbf6bbe10",
+    "url": "/react-app/index.html"
+  }
+];
